fcbec2fa9db774caac59dc7475de9bd4ea86be5b
Stephanie Smithson <165820568+stephaniesmithson-8451@users.noreply.github.com>
2024-12-03 11:07:12 -0600

    Added day 3 skeleton

---
+++ Day 3/memory.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Memory {
+
+    public File data;
+
+    public String getValidString(String gibberish) {
+        String decoded_gibberish = "";
+
+        return decoded_gibberish;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
