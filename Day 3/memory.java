7fb01380aa567c7fd135d95aef5e2d876629cba5
Stephanie Smithson <stephaniesmithson16@gmail.com>
2024-12-03 20:57:51 -0600

    Completed day 3 part 1

--- Day 3/memory.java
+++ Day 3/memory.java
@@ -1,20 +1,50 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Memory {
 
-    public File data;
+    public static String readGibberishFile(String path) {
+        String gibberish = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                gibberish += line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    public String getValidString(String gibberish) {
-        String decoded_gibberish = "";
+        return gibberish;
+    }
 
-        return decoded_gibberish;
+    public static int parseGibberish(String gibberish) {
+        int total = 0;
+
+        Pattern pattern = Pattern.compile("mul[(]\\d{1,3}[,]\\d{1,3}[)]");
+        Matcher matcher = pattern.matcher(gibberish);
+
+        System.out.println("Gibberish:\n" + gibberish + "\n");
+
+        while (matcher.find()) {
+            String match = matcher.group();
+            String numbers = match.substring(match.indexOf("(") + 1, match.indexOf(")"));
+            int firstNum = Integer.parseInt(numbers.split(",")[0]);
+            int secondNum = Integer.parseInt(numbers.split(",")[1]);
+            System.out.println("\nFirst number: " + firstNum + "\tSecond number: " + secondNum);
+            total += firstNum * secondNum;
+            System.out.println("Product: " + (firstNum * secondNum));
+        }
+
+        return total;
     }
 
     public static void main(String[] args) {
-
-
+        String gibberish = readGibberishFile("data.txt");
+        int total = parseGibberish(gibberish);
+        System.out.println("\nThe total is: " + total);
     }
 
 }
